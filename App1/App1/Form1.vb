40dcb3733f71a709f3c823f228bf887783e7f226
André Dias Neto <andre1parallel@gmail.com>
2018-10-31 15:13:09 -0300

    commit 02

--- App1/App1/Form1.vb
+++ App1/App1/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
